7a5aa640fdd587dc7fe3000280077d18123315b1
panpeng <panpenglyz@126.com>
2018-09-28 20:35:45 +0800

    add LindedList

---
+++ src/com/structure/array/list/LinkedList.java
@@ -0,0 +1,32 @@
+package com.structure.array.list;
+
+/**
+ * @author pp-sir
+ * @date 2018/9/27 10:36
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+}
