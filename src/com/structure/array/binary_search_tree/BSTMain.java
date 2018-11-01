61e9399c49ddfe2d6d22d282bf7af48583bfbeb2
panpeng <panpenglyz@126.com>
2018-11-01 21:03:39 +0800

    test BST by LeetCode 804

--- src/com/structure/array/binary_search_tree/BSTMain.java
+++ src/com/structure/array/binary_search_tree/BSTMain.java
@@ -7,10 +7,34 @@ package com.structure.array.binary_search_tree;
 public class BSTMain {
 
     public static void main(String[] args) {
-        BST<Integer> bst = new BST<>();
-        bst.add(10);
 
-        System.out.println(bst.toString());
+        String[] words = {"gin", "zen", "gig", "msg"};
+
+        BSTMain main = new BSTMain();
+        main.uniqueMorseRepresentations(words);
+
+    }
+
+    /**
+     * LeetCode 804
+     * @param args
+     */
+    public void uniqueMorseRepresentations(String[] args) {
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+                "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+        BST<String> stringBST = new BST<>();
+        for (String word : args) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                String s = codes[word.charAt(i) - 'a'];
+                stringBuilder.append(s);
+            }
+            stringBST.add(stringBuilder.toString());
+            System.out.println(stringBuilder.toString());
+        }
+        System.out.println(stringBST.getSize());
+
     }
 
 }
